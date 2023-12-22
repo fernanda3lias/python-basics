eab9d0d573955431b86dae2a776c86acd3ee47a5
Fernanda <fernanda.eliasct@gmail.com>
2023-12-22 13:43:31 -0300

    add dot env example files

---
+++ modules/environment_variables/main.py
@@ -0,0 +1,11 @@
+# Environment variables with Python
+# doc: https://pypi.org/project/python-dotenv/
+# Never send your .env file to a remote repo
+# Always create .env-example
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+#print(os.environ)
+print(os.getenv("DB_PASSWORD"))
