4759524678c14b2d9036cb879844baec719e8e69
Fernanda <fernanda.eliasct@gmail.com>
2023-12-27 13:33:04 -0300

    add LIFO and FIFO

---
+++ modules/deque.py
@@ -0,0 +1,15 @@
+# Deque - Working with LIFO and FIFO
+# deque - Double-ended queue
+#
+# LIFO and FIFO
+# Stack and Queue
+
+# LIFO (Last In First Out)
+# Stack
+# Means that the last item to enter will be the first to exit (list)
+# Article:
+# https://www.otaviomiranda.com.br/2020/pilhas-em-python-com-listas-stack/
+# Video:
+# https://youtu.be/svWVHEihyNI
+# Removing items from the end: O(1) Constant time
+# Removing items from the beginning: O(n) Linear time
